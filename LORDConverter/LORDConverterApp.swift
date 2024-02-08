4d69ee4ae1d64afb201b60f2be964742312f97a4
Higor <higorjugler@hotmail.com>
2024-02-08 09:01:03 -0300

    Initial Commit

---
+++ LORDConverter/LORDConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LORDConverterApp.swift
+//  LORDConverter
+//
+//  Created by Higor Jugler on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LORDConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
