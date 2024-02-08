b4de8dce529fcfdc2b4c308229920ec7fc2a3513
Higor <higorjugler@hotmail.com>
2024-02-08 15:33:35 -0300

    Projeto de estudo em iOS

--- LORDConverter/ContentView.swift
+++ LORDConverter/ContentView.swift
@@ -8,14 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeInfo = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                // Pracing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    
+                    VStack {
+                        
+                        HStack {
+                            // Left Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // Currency text
+                            Text("Silver price")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        
+                        // Text fild
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    VStack {
+                        // Right currency section
+                        HStack {
+                            // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // Currency text
+                            Text("Silver price")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        
+                        // Text fild
+                        TextField("Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
+                
+            }
+            //            .border(.blue)
         }
-        .padding()
     }
 }
 
